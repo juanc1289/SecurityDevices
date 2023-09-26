31e68e3ce7c7c924934e7ee44fbe05eb62db76eb
juanc1289 <juan.cartagena94@gmail.com>
2023-09-26 19:49:37 +0100

    gps loop working

--- pri.py
+++ pri.py
@@ -6,3 +6,10 @@ b = 2023
 now = datetime.now()
 dt_string = now.strftime("%Y%m%d_%H%M%S")
 print(f'DS{a:04d}_{dt_string}')
+
+
+file1 = open(f'../../ubicaciones/DS{a:04d}_{dt_string}.txt',"a")
+
+file1.write("am")
+
+file1.close()
