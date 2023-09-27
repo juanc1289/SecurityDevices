0c732ce146768dcf329d8921402edd2e19508c14
juanc1289 <juan.cartagena94@gmail.com>
2023-09-27 22:11:14 +0100

    launcher working

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # launcher.sh
-# navigate home, then to the directory, then execute the python script
-
+# navigate home, then to the directory, then execute the python script 
+#[sudo python gps3_powerkey.py ]
+#[sudo python camera4_video_loop.py]
 
 cd /
 cd home/juanc/Desktop/git/SecurityDevices/
-sleep 60 && sudo python gps2_loop_clean.py
+sleep 6 && lxterminal -e sudo python gps3_powerkey.py && lxterminal -e sudo python camera4_video_loop.py
